f45ea6269d679ac677401de6cc58d46a91373faa
Roboboboi <60798716+Roboboboi@users.noreply.github.com>
2020-10-09 23:07:22 -0400

    Add files via upload

---
+++ heightmap/config.js
@@ -0,0 +1,15 @@
+var images = [
+"coolPattern3.png",
+"hm3.png",
+"hm4.png",
+"hm5.png",
+"plateau 1.png",
+"volcano.png",
+"wKS4ua4.png",
+];
+// The low res images (they should all be the same size)
+var src_min = "images/min/";
+// The max resolution images (Use the same if there are no higher resolution ones available)
+var src_max = "images/max/";
+// The local source for the image (used in commands)
+var src_local = "file://";
